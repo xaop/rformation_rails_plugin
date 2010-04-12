4f140c4e27c8f6b86cc9d926bf2b7ac5933fe101
Peter Vanbroekhoven <peter@xaop.com>
2010-04-12 16:10:58 +0200

    Fixed commit TODO: manage cache because now it can grow infinite

--- lib/rformation_plugin.rb
+++ lib/rformation_plugin.rb
@@ -9,25 +9,35 @@ module RFormation
     def compile(template)
       filename = template.filename
       relative_filename = filename.sub(/\A#{Regexp.escape(RAILS_ROOT)}\//, "").sub(/\.html\.rfrm/, '')
+      id = rand(2**64)
       %{
         __lists_of_values__ = ::RFormation::Plugin::ListsOfValues.new(binding)
         __data__ = ::RFormation::Plugin::DataObjectContainer.new(binding)
         form = eval(<<-RFRM_END, binding, "FORM_DSL")
           ::RFormation::Form.new(:filename => #{filename.inspect}, :lists_of_values => __lists_of_values__) do
-            object '$rformation$#{relative_filename}' do
+            object '$rformation$#{relative_filename}$#{id}' do
               #{::File.read(filename).split(/\n/).map { |l| l.inspect[1..-2] }.join("\n")}
             end
           end
         RFRM_END
+        RFormation::Plugin.register_form(#{filename.inspect}, #{id}, form)
         form.to_html(:data => __data__, :lists_of_values => __lists_of_values__)
       }
+    end
+    
+    def self.register_form(filename, id, form)
+      @@form_cache[[filename, id]] = form
+    end
+    
+    def self.get_form(filename, id)
+      @form_cache[[filename, id]]
     end
 
     def self.clean_params(params)
       form_data = {}
       params.keys.each do |key|
-        if /\A\$rformation\$(.*)/ === key.to_s
-          form = get_form(RAILS_ROOT + "/" + $1 + ".html.rfrm")
+        if /\A\$rformation\$(.*)$\d+/ === key.to_s
+          form = get_form(RAILS_ROOT + "/" + $1 + ".html.rfrm", $2.to_i)
           begin
             cleaned_data = form.validate_form({ key => params[key] })
             form_data.merge!(cleaned_data[key.to_s])
