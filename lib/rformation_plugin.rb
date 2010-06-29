46b27105408d15eda4f41a29e87307459e57a0a7
Joeri Samson <joeri@xaop.com>
2010-06-29 13:48:58 +0200

    Allow rformation to work with load balancing/server restarts

--- lib/rformation_plugin.rb
+++ lib/rformation_plugin.rb
@@ -8,19 +8,12 @@ module RFormation
 
     def compile(template)
       filename = template.filename
-      relative_filename = filename.sub(/\A#{Regexp.escape(RAILS_ROOT)}\//, "").sub(/\.html\.rfrm/, '')
-      id = rand(2**64)
+      form, id = RFormation::Plugin.create_form(filename)
+
       %{
         __lists_of_values__ = ::RFormation::Plugin::ListsOfValues.new(binding)
         __data__ = ::RFormation::Plugin::DataObjectContainer.new(binding)
-        form = eval(<<-RFRM_END, binding, "FORM_DSL")
-          ::RFormation::Form.new(:filename => #{filename.inspect}, :lists_of_values => __lists_of_values__) do
-            object '$rformation$#{relative_filename}$#{id}' do
-              #{::File.read(filename).split(/\n/).map { |l| l.inspect[1..-2] }.join("\n")}
-            end
-          end
-        RFRM_END
-        RFormation::Plugin.register_form(#{filename.inspect}, #{id}, form)
+        form = RFormation::Plugin.get_form(#{filename.inspect}, #{id.inspect})
         form.to_html(:data => __data__, :lists_of_values => __lists_of_values__)
       }
     end
@@ -33,19 +26,55 @@ module RFormation
       @@form_cache[[filename, id]]
     end
 
+    def self.create_form(filename)
+      if ::File.exist? filename
+        id = Digest::SHA1.file(filename).hexdigest
+        unless form = get_form(filename.inspect, id)
+          relative_filename = filename.sub(/\A#{Regexp.escape(RAILS_ROOT)}\//, "").sub(/\.html\.rfrm/, '')
+          source = <<-RFRM_END
+            ::RFormation::Form.new(:filename => #{filename.inspect}, :lists_of_values => proc { true }) do; object '$rformation$#{relative_filename}$#{id}' do; #{::File.read(filename)}
+            end; end
+          RFRM_END
+          form = eval(source, binding, "FORM_DSL")
+          RFormation::Plugin.register_form(filename, id, form)
+        end
+        [form, id]
+      else
+        nil
+      end
+    end
+
     def self.clean_params(params)
       form_data = {}
       params.keys.each do |key|
-        if /\A\$rformation\$(.*)\$(\d+)\z/ === key.to_s
-          form = get_form(RAILS_ROOT + "/" + $1 + ".html.rfrm", $2.to_i)
-          begin
-            cleaned_data = form.validate_form({ key => params[key] })
-            form_data.merge!(cleaned_data[key.to_s])
-          rescue RFormation::ValidationError => e
-            (form_data[:form_errors] ||= {}).merge!(e.errors)
-            form_data.merge!(e.data[key.to_s])
+        if /\A\$rformation\$(.*)\$([a-fA-F0-9]+)\z/ === key.to_s
+          filename = Rails.root.join($1 + ".html.rfrm").to_s
+          if form = get_form(filename, $2)
+            validate = true
+          else
+            form, id = create_form(filename)
+            if form && id == $2
+              validate = true
+            else
+              form_data[:form_errors] = {}
+              if form
+                form_data[:form_errors]["This form"] ||= "was changed on the server, please refresh this page and fill in the form again"
+              else
+                form_data[:form_errors]["This form"] ||= "does not exist"
+              end
+              validate = false
+            end
           end
-          params.delete(key)
+          if validate
+            begin
+              cleaned_data = form.validate_form({ key => params[key] })
+              form_data.merge!(cleaned_data[key.to_s])
+            rescue RFormation::ValidationError => e
+              (form_data[:form_errors] ||= {}).merge!(e.errors)
+              form_data.merge!(e.data[key.to_s])
+            end
+            params.delete(key)
+          end
         end
       end
       params.merge!(form_data)
